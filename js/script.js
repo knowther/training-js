ede38c2083e1b89982f6b5700b5e1af637b90ef8
knowther <johnnywesley.contato@outlook.com>
2020-11-30 19:03:10 -0300

    Base inicial criada, alguns comandos JS funcionais

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function change(){
+    var nome_p = prompt("Qual seu nome?");
+    document.getElementById("texto").innerHTML = `Olá ${nome_p}, o que mais deseja fazer?`;
+    document.getElementById("initext").innerText = `${nome_p}, essa são as opções que você tem para interagir:`;
+}
+
+function createbutton(){
+
+var newbutton = document.createElement("button");
+var pai_element = document.body;
+var textbt = document.createTextNode("Exibir uma foto minha.")
+newbutton.appendChild(textbt);
+pai_element.appendChild(newbutton);
+
+
+}
