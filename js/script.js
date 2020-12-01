1c0851344d886c59c0336b32e41de70c0eb4b67c
knowther <johnnywesley.contato@outlook.com>
2020-12-01 00:37:29 -0300

    Agora é possível trocar o nome

--- js/script.js
+++ js/script.js
@@ -7,18 +7,30 @@ function change(){
 function createbutton(){
 
 var newbutton = document.createElement("button");
-var pai_element = document.body;
+var pai_element = document.getElementById("main");
 var textbt = document.createTextNode("Exibir uma foto minha.")
+var trocar_N = document.createElement("button")
+var paielementN = document.getElementById("main")
+var texTrocar = document.createTextNode("Trocar nome")
 
 newbutton.appendChild(textbt);
+trocar_N.appendChild(texTrocar);
+paielementN.appendChild(trocar_N);
 
 pai_element.appendChild(newbutton);
+
+trocar_N.onclick = function trocarN(){
+    var nome_p = prompt("Qual seu nome?");
+    document.getElementById("texto").innerHTML = `Olá ${nome_p}, o que mais deseja fazer?`;
+    document.getElementById("initext").innerText = `${nome_p}, essa são as opções que você tem para interagir:`;
+}
+
 var oneclick = false;
 newbutton.onclick = function photo() {
     
     if(!oneclick){
         var newbutton2 = document.createElement("button");
-    var pai_element2 = document.body;
+    var pai_element2 = document.getElementById("main");
     var textbt2 = document.createTextNode("Deixar foto em preto e branco")
     newbutton2.appendChild(textbt2);
     pai_element2.appendChild(newbutton2);
